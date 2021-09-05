d521d5bf026c1a73cf1a2c8c0ff957aa9c23d30c
Fernando Flores <nbafernandoflores8@gmail.com>
2021-09-05 14:35:47 -0600

    service test

---
+++ src/app/hola-mundo3.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { HolaMundo3Service } from './hola-mundo3.service';
+
+describe('HolaMundo3Service', () => {
+  let service: HolaMundo3Service;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(HolaMundo3Service);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
