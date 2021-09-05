be81e46ac75395fd2bdb87c5f887eb7581c4374b
Fernando Flores <nbafernandoflores8@gmail.com>
2021-09-05 14:51:43 -0600

    Update hola-mundo2.service.spec.ts

--- src/app/hola-mundo2.service.spec.ts
+++ src/app/hola-mundo2.service.spec.ts
@@ -4,7 +4,7 @@ import { HolaMundo2Service } from './hola-mundo2.service';
 
 describe('HolaMundo2Service', () => {
   let service: HolaMundo2Service;
-
+ //change
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(HolaMundo2Service);
